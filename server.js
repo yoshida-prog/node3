1b4a1efb3ac2762567ae2955baf0faae9045dfbe
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-21 22:39:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,16 +14,41 @@ app.use(bodyParser.json());
 
 const options = {
   method: 'GET',
-  json: true,
-  url: URL
+  url: URL,
+  headers: {
+    'Content-type': 'application/json'
+  },
+  json: true
 };
+
+class Quiz {
+  constructor(category, type, difficulty, question, correct_answer, incorrect_answers) {
+    this.category = category;
+    this.type = type;
+    this.difficulty = difficulty;
+    this.question = question;
+    this.correct_answer = correct_answer;
+    this.incorrect_answers = incorrect_answers;
+  }
+}
 
 app.get('/', urlencodedParser, (req, res) => {
   request(options, (err, response, body) => {
-    const results = body.results[0]
-    const category = results.category;
-    console.log(body.results[0]);
-    res.render('index', {category});
+    const quizDatas = body.results;
+    let quizArray = [];
+    for (let index in quizDatas) {
+      let quiz = new Quiz(
+        quizDatas[index].category,
+        quizDatas[index].type,
+        quizDatas[index].difficulty,
+        quizDatas[index].question,
+        quizDatas[index].correct_answer,
+        quizDatas[index].incorrect_answers
+      );
+      quizArray.push(quiz);
+    }
+    quizArray = JSON.stringify(quizArray);
+    res.render('index', {quizArray});
   });
 });
 
